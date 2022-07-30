4e414c6677a1888d426e9c0f28b305a9c4270088
alukard367 <motivatedCoder367@gmail.com>
2022-07-30 01:53:15 -0700

    re-Init

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'));
+const menuBtn = document.querySelector(".menu-btn")
+const mainMenu = document.querySelector(".main-menu")
+
+const toggleContent = document.querySelector(".toggle-content")
+const readMoreBtn = document.querySelector(".readmore")
+
+menuBtn.addEventListener("click", ()=> {
+    mainMenu.classList.toggle("show")
+    document.body.classList.toggle("no-scroll")
+})
+
+
+readMoreBtn.addEventListener("click", ()=> {
+    toggleContent.classList.toggle("showmore")
+    readMoreButton()
+})
+
+function readMoreButton() {
+    if(readMoreBtn.innerText == "READ MORE") {
+        readMoreBtn.innerText = "READ LESS..."
+    } else if(readMoreBtn.innerText = "READ LESS..."){
+        readMoreBtn.innerText = "READ MORE"
+    } 
+}
