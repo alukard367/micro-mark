3598987f5cf65707a9dabc7cec0d644727120033
alukard367 <motivatedCoder367@gmail.com>
2022-08-19 11:56:01 -0700

    just saving my work

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 const menuBtn = document.querySelector(".menu-btn")
 const mainMenu = document.querySelector(".main-menu")
 
-const toggleContent = document.querySelectorAll(".toggle-content")
-const readMoreBtn = document.querySelectorAll(".readmore")
+const expandsMore = document.querySelectorAll("[expand-more]")
 
 menuBtn.addEventListener("click", ()=> {
     mainMenu.classList.toggle("show")
     document.body.classList.toggle("no-scroll")
 })
 
-
-readMoreBtn.addEventListener("click", ()=> {
-    toggleContent.classList.toggle("showmore")
-    readMoreButton()
-})
-
-function readMoreButton() {
-    if(readMoreBtn.innerText == "READ MORE") {
-        readMoreBtn.innerText = "READ LESS..."
-    } else if(readMoreBtn.innerText = "READ LESS..."){
-        readMoreBtn.innerText = "READ MORE"
-    } 
+function expand() {
+    const showContent = document.getElementById(this.dataset.target)
+    if(showContent.classList.contains("expand-active")) {
+        this.innerHTML = this.dataset.showtext
+    } else {
+        this.innerHTML = this.dataset.hidetext
+    }
+    showContent.classList.toggle("expand-active")
 }
+
+expandsMore.forEach(expandMore => {
+    expandMore.addEventListener("click", expand)
+})
